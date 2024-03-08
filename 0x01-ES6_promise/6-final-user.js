e0be1e1ae1d9e718b04963a79a754897b4cd8f54
Yeeloman <yami.onlyme@gmail.com>
2024-03-09 00:18:28 +0100

    add task 10

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,20 +1,21 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, filename) {
-  const promises = [
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  try {
+    const [userResult, photoResult] = await Promise.all([
+      signUpUser(firstName, lastName),
+      uploadPhoto(fileName),
+    ]);
 
-    signUpUser(firstName, lastName)
-      .then((value) => ({ status: 'fulfilled', value }))
-      .catch((error) => ({ status: 'rejected', value: error })),
-
-    uploadPhoto(filename)
-      .then((value) => ({ status: 'fulfilled', value }))
-      .catch((error) => ({ status: 'rejected', value: error })),
-
-  ];
-
-  return Promise.allSettled(promises)
-    .then((results) => results
-      .map(({ status, value }) => ({ status, value })));
+    return [
+      { status: 'fulfilled', value: userResult },
+      { status: 'fulfilled', value: photoResult },
+    ];
+  } catch (error) {
+    return [
+      { status: 'rejected', value: error },
+      { status: 'rejected', value: error },
+    ];
+  }
 }
