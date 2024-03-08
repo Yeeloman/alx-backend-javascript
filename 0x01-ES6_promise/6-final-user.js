444272c6738cb4defb508e502c99c1fca3abdeb0
Yeeloman <yami.onlyme@gmail.com>
2024-03-09 00:22:38 +0100

    add task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,21 +1,19 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  try {
-    const [userResult, photoResult] = await Promise.all([
-      signUpUser(firstName, lastName),
-      uploadPhoto(fileName),
-    ]);
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpUserPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
 
-    return [
-      { status: 'fulfilled', value: userResult },
-      { status: 'fulfilled', value: photoResult },
-    ];
-  } catch (error) {
-    return [
-      { status: 'rejected', value: error },
-      { status: 'rejected', value: error },
-    ];
-  }
+  return Promise.allSettled([signUpUserPromise, uploadPhotoPromise]).then((values) => {
+    const result = [];
+    values.forEach((el) => {
+      if (el.status === 'fulfilled') {
+        result.push({ status: el.status, value: el.value });
+      } else {
+        result.push({ status: el.status, value: `${el.reason}` });
+      }
+    });
+    return result;
+  });
 }
