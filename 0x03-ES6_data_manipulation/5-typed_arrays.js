2d48e4212d630dc6a9a8cc752402cc63c2f14dcc
Yeeloman <yami.onlyme@gmail.com>
2024-03-11 22:06:13 +0000

    done with task 5 i guess
     a function named createInt8TypedArray that returns a new ArrayBuffer with an Int8 value at a specific position.
    
    It should accept three arguments: length (Number), position (Number), and value (Number).
    
    If adding the value is not possible the error Position outside range should be thrown.

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,11 @@
+const createInt8TypedArray = (length, position, value) => {
+  if (position < 0 || position >= length) {
+    throw Error('Position outside range');
+  }
+  const buf = new ArrayBuffer(length);
+  const view = new Int8Array(buf, 0, length);
+  view.set([value], position);
+  return view;
+};
+
+export default createInt8TypedArray;
