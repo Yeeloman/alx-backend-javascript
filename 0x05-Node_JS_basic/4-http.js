84a4c798dbb1403f10b7301260d5bdaa185cfe77
Yeeloman <yami.onlyme@gmail.com>
2024-05-01 00:10:59 +0100

    check

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,16 +1,15 @@
-#!/usr/bin/node
-
 const http = require('http');
 
-const hostname = 'localhost';
+const hostname = '127.0.0.1';
 const port = 1245;
 
-const app = http.createServer((req, resp) => {
-  resp.statusCode = 200;
-  resp.setHeader('Content-Type', 'text/plain');
-  resp.end('Hello Holberton School');
+const app = http.createServer((request, response) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  response.end('Hello Holberton School!');
 });
 
-app.listen(port, hostname, () => {});
+app.listen(port, hostname, () => {
+});
 
 module.exports = app;
