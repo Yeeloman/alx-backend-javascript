dac80a4c05f6c33b0127c915b223f9c1c1507f81
Yeeloman <yami.onlyme@gmail.com>
2024-05-01 14:56:59 +0100

    check

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,30 +1,30 @@
-const ass = require('assert');
+const assert = require('assert');
 const { it, describe } = require('mocha');
 const calculateNumber = require('./0-calcul');
 
-describe('#calculate and round a number', function () {
-  it('checking if numbers round', function () {
+describe('#calculate and round a number', function() {
+  it('checking if numbers round', function() {
     const res = calculateNumber(1, 2);
-    ass.strictEqual(res, 3);
+    assert.strictEqual(res, 3);
   });
-  it('checking if numbers round', function () {
+  it('checking if numbers round', function() {
     const res = calculateNumber(1.3, 2.1);
-    ass.strictEqual(res, 3);
+    assert.strictEqual(res, 3);
   });
-  it('checking if numbers round', function () {
+  it('checking if numbers round', function() {
     const res = calculateNumber(1.6, 2.9);
-    ass.strictEqual(res, 5);
+    assert.strictEqual(res, 5);
   });
-  it('checking if numbers round', function () {
+  it('checking if numbers round', function() {
     const res = calculateNumber(0, 0);
-    ass.strictEqual(res, 0);
+    assert.strictEqual(res, 0);
   });
-  it('checking if numbers round', function () {
+  it('checking if numbers round', function() {
     const res = calculateNumber(-1.7, -1.8);
-    ass.strictEqual(res, -4);
+    assert.strictEqual(res, -4);
   });
-  it('checking if numbers round', function () {
+  it('checking if numbers round', function() {
     const res = calculateNumber(-1.3, -1.3);
-    ass.strictEqual(res, -2);
+    assert.strictEqual(res, -2);
   });
 });
--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,65 +1,65 @@
-const ass = require('assert');
+const assert = require('assert');
 const { it, describe } = require('mocha');
 const calculateNumber = require('./1-calcul');
 
-describe('all calculations', function () {
-  describe('SUM calculation', function () {
-    it('checking if numbers round', function () {
+describe('all calculations', function() {
+  describe('SUM calculation', function() {
+    it('checking if numbers round', function() {
       const res = calculateNumber('SUM', 1, 2);
-      ass.strictEqual(res, 3);
+      assert.strictEqual(res, 3);
     });
-    it('checking if numbers round', function () {
+    it('checking if numbers round', function() {
       const res = calculateNumber('SUM', 1.3, 2.1);
-      ass.strictEqual(res, 3);
+      assert.strictEqual(res, 3);
     });
-    it('checking if numbers round', function () {
+    it('checking if numbers round', function() {
       const res = calculateNumber('SUM', 1.6, 2.9);
-      ass.strictEqual(res, 5);
+      assert.strictEqual(res, 5);
     });
-    it('checking if numbers round', function () {
+    it('checking if numbers round', function() {
       const res = calculateNumber('SUM', 0, 0);
-      ass.strictEqual(res, 0);
+      assert.strictEqual(res, 0);
     });
-    it('checking if numbers round', function () {
+    it('checking if numbers round', function() {
       const res = calculateNumber('SUM', -1.7, -1.8);
-      ass.strictEqual(res, -4);
+      assert.strictEqual(res, -4);
     });
-    it('checking if numbers round', function () {
+    it('checking if numbers round', function() {
       const res = calculateNumber('SUM', -1.3, -1.3);
-      ass.strictEqual(res, -2);
+      assert.strictEqual(res, -2);
     });
   });
-  describe('SUBSTRACT numbers', function () {
-    it('checking substraction of rounded numbers', function () {
+  describe('SUBSTRACT numbers', function() {
+    it('checking substraction of rounded numbers', function() {
       const res = calculateNumber('SUBTRACT', 2, 1);
-      ass.strictEqual(res, 1);
+      assert.strictEqual(res, 1);
     });
-    it('checking substraction of rounded numbers', function () {
+    it('checking substraction of rounded numbers', function() {
       const res = calculateNumber('SUBTRACT', 2.2, 1.4);
-      ass.strictEqual(res, 1);
+      assert.strictEqual(res, 1);
     });
-    it('checking substraction of rounded numbers', function () {
+    it('checking substraction of rounded numbers', function() {
       const res = calculateNumber('SUBTRACT', 2.8, 1.4);
-      ass.strictEqual(res, 2);
+      assert.strictEqual(res, 2);
     });
-    it('checking substraction of rounded numbers', function () {
+    it('checking substraction of rounded numbers', function() {
       const res = calculateNumber('SUBTRACT', 2.9, 1.7);
-      ass.strictEqual(res, 1);
+      assert.strictEqual(res, 1);
     });
   });
 
-  describe('DIVIDE numbers', function () {
-    it('checking divide of rounded numbers', function () {
+  describe('DIVIDE numbers', function() {
+    it('checking divide of rounded numbers', function() {
       const res = calculateNumber('DIVIDE', 2.2, 1.4);
-      ass.strictEqual(res, 2);
+      assert.strictEqual(res, 2);
     });
-    it('checking divide of rounded numbers', function () {
+    it('checking divide of rounded numbers', function() {
       const res = calculateNumber('DIVIDE', 2.9, 1.8);
-      ass.strictEqual(res, 1.5);
+      assert.strictEqual(res, 1.5);
     });
-    it('checking divide of rounded numbers', function () {
+    it('checking divide of rounded numbers', function() {
       const res = calculateNumber('DIVIDE', 2.2, 0);
-      ass.strictEqual(res, 'Error');
+      assert.strictEqual(res, 'Error');
     });
   });
 });
