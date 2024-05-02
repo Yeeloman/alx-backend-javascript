5eba5e3c63cc8071632085104f742677d772d434
Yeeloman <yami.onlyme@gmail.com>
2024-05-02 02:02:02 +0100

    task 9

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -1,1 +1,16 @@
-#!/usr/bin/node
+const express = require('express');
+const app = express();
+const port = 7865;
+
+app.get('/', (_, res) => {
+  res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id([0-9]+)', (req, res) => {
+  res.send(`Payment methods for cart ${req.params.id}`);
+});
+app.listen(port, () => {
+  console.log('API available on localhost port 7865');
+});
+
+module.exports = app;
